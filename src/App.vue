0dc57255666c0502dfd491b0e787abe588692f76
Patrick Schilf <pschilf@unb.ca>
2024-09-23 15:13:02 -0300

    Test page layout, style

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,10 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="grid grid-cols-6 gap-8 pb-8 h-full overflow-y-scroll">
+    <div v-for="(_, index) in Array.from({ length: 24 })"
+      class="dark:bg-slate-600 bg-slate-200 border-slate-300 border dark:border-slate-500 rounded-xl p-6">
+      <HelloWorld />
+    </div>
   </div>
-  <HelloWorld />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
