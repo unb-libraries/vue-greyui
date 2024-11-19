aa8b49829d4b2871aa703275cb9748587174c5d3
Patrick Schilf <pschilf@unb.ca>
2024-11-19 10:24:43 -0400

    feat: useInputNumber
    Reusable composable for number inputs with configurable decimals, dynamic step size.

--- lib/composables/__tests__/index.spec.ts
+++ lib/composables/__tests__/index.spec.ts
@@ -2,7 +2,8 @@ import { expect, test } from "vitest"
 
 test('export all composables except useInput', async () => {
   const index = await import('../index')
-  expect(index).toHaveProperty('useInputText')
-  expect(index).toHaveProperty('useInputValidation')
   expect(index).toHaveProperty('useInputAttrs')
+  expect(index).toHaveProperty('useInputText')
+  expect(index).toHaveProperty('useInputNumber')
+  expect(index).toHaveProperty('useInputValidation')
 })
--- lib/composables/index.ts
+++ lib/composables/index.ts
@@ -1,6 +1,7 @@
-export * from "./useInputText"
-export * from "./useInputValidation"
 export * from "./useInputAttrs"
+export * from "./useInputText"
+export * from "./useInputNumber"
+export * from "./useInputValidation"
 
 export type EventHandler = (event: Event) => void
 export type InputHandler<T = unknown> = (handler: (value: T) => void) => EventHandler
